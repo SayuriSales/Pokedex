d29cab165195718f9750f25a3ba70f2344c58747
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-03 22:59:38 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sayurisales/pokedex/ListaActivity.kt
@@ -0,0 +1,36 @@
+package br.com.sayurisales.pokedex
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import br.com.sayurisales.pokedex.model.Pokemon
+import kotlinx.android.synthetic.main.activity_lista.*
+
+class ListaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+
+        rvPokemons.adapter = ListaPokemonAdapter(getPokemons(),
+                this, {pokemon -> Toast.makeText(this, pokemon.nome, Toast.LENGTH_LONG).show()})
+
+        rvPokemons.layoutManager = LinearLayoutManager (this)
+    }
+
+    fun getPokemons() : List<Pokemon>{
+        return listOf(
+                getPsyduck()
+        )
+    }
+
+    fun getPsyduck() : Pokemon{
+        return  Pokemon(1,
+                "Psyduck",
+                "Water",
+                "blablabla",
+                R.drawable.splash)
+    }
+
+}
